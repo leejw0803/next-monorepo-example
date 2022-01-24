91e8246886ecc212bb9126e5aedd9f5e975a8658
Lee Jungwoo <leejw0803@konkuk.ac.kr>
2022-01-24 12:31:10 +0900

    yarn workspace + lerna 테스트

---
+++ packages/Input/lib/Input.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Input = styled.input`
+  border: none;
+  border-radius: 4px;
+  font-size: 1rem;
+  font-weight: 300;
+  padding: 9px 36px;
+  border: 1px solid #fe4784;
+`;
